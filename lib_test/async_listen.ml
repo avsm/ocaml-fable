0f075414b837f1a066641778d33422cbf0a3b017
Anil Madhavapeddy <anil@recoil.org>
2013-02-24 14:34:51 +0000

    add listener test

---
+++ lib_test/async_listen.ml
@@ -0,0 +1,47 @@
+(*
+ * Copyright (c) 2013 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+
+open Core.Std
+open Async.Std
+
+open Fable_async
+
+let f flow =
+  Log.Global.info "New flow";
+  let flow,rd,wr = map_flow_to_string flow in
+  Log.Global.sexp flow sexp_of_flow_state;
+  Pipe.iter rd ~f:(fun s -> 
+    Log.Global.debug "read %s" s;
+    Pipe.write wr s
+  )
+
+let t =
+  Log.Global.info "Start";
+  let ctx = init () in
+  let uri = Uri.of_string "http://anil.recoil.org:5555" in
+  listen ~ctx ~uri ~f
+  >>= fun listener ->
+  Log.Global.info "Listener started";
+  after (Time.Span.of_sec 10.)
+  >>= fun () ->
+  Log.Global.info "Listening shutting down";
+  close_listener listener
+
+let _ =
+  Log.Global.set_level `Debug;
+  Log.Global.set_output [Log.Output.screen];
+  never_returns (Scheduler.go ())
